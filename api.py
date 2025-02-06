395d9df2637b72ae5b248f662ed01844618b6780
Alejandro Otermin <pl198793@ahschool.com>
2025-02-06 10:13:10 -0500

    web stuff

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template, Response, redirect, url_for
+
 app = Flask(__name__)
+web = Flask(__name__, template_folder='Templates')
 
 @app.route('/')
 def home():
    return render_template('index.html')
 if __name__ == '__main__':
    app.run()
+
+@web.route('/admin')
+def index():
+    return render_template('index.html')
